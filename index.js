34dd6c4921b0357e990fff98ced6655b1cc867ef
Michał Budzyński <michal@virtualdesign.pl>
2019-04-02 16:21:03 +0200

    :vhs: pass dirpath from the cli

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const Slack = require('slack-node');
-// console.log(process.argv);
+const path = require('path');
+
 const apiToken = process.argv[2];
+const demos_path = process.argv[3] || path.join(__dirname, '..', 'demos');
 const stat_parser = require('./stat-parser');
 
 slack = new Slack(apiToken);
 
-// slack.api('chat.postMessage', {
-//     text: "Kolejny mecz zakończony. Wyniki: *budzor* 10 : 3 maciek niszczyciel",
-//     username: 'Jeloquake',
-//     icon_emoji: ":quake:",
-//     channel: '#t2'
-// }, function (err, response) {
-//     console.log(response);
-// });
+stat_parser(demos_path)
+.then(stats => {
+    const players = stats.players.reduce((memo, val, i) => {
+        if (i === 0) {
+            val.name = `*${val.name}*`
+        };
+
+        memo.push(`${val.name} (*${val.frags}*)`);
+        return memo;
+    }, []).join(' :vs: ');
+
+    slack.api('chat.postMessage', {
+        text: `*${stats.map.toUpperCase()}* deathmatch results: ${players}`,
+        username: 'Jeloquake',
+        icon_emoji: ":quake:",
+        channel: '#t2'
+    }, function (err, response) {
+        console.log(response);
+    });
+});
--- stat-parser.js
+++ stat-parser.js
@@ -1,11 +1,42 @@
 const path = require('path')
 const fs = require('fs')
+const xml = require('xml2js').parseString;
 
-const dirpath = path.join(__dirname, '/demos')
-const processed_files = fs.readFileSync('processed_files').toString().split('\n');
-const files = fs.readdirSync(dirpath);
-const new_file = files.filter(el => /\.txt$/.test(el)).find(el => !processed_files.includes(el));
+module.exports = (demos_path) => {
+    return new Promise((resolve, reject) => {
+        const dirpath = demos_path;
+        const processed_files = fs.readFileSync('processed_files').toString().split('\n');
+        const files = fs.readdirSync(dirpath);
+        const new_file = files.filter(el => /\.txt$/.test(el)).find(el => !processed_files.includes(el));
+        if (!new_file) {
+            return;
+        }
+        const stats = fs.readFileSync(path.join(dirpath, new_file));
 
-const stats = fs.readFileSync(path.join(dirpath, new_file));
+        xml(stats.toString(), (err, res) => {
+            if (err) {
+                console.log('Nope: ', err)
+                return;
+            }
 
-console.log(stats.toString());
+            try {
+                const map = res.match.$.map;
+                console.log(map);
+                const players = res.match.players[0].player.map(player => ({
+                    name: player.$.name,
+                    frags: parseInt(player.$.frags, 10)
+                })).sort((a, b) => a.frags < b.frags);
+
+                fs.appendFileSync('processed_files', new_file);
+
+                resolve({
+                    players,
+                    map
+                });
+
+            } catch(e) {
+                console.log('Nope: ', err)
+            }
+        });
+    });
+}
